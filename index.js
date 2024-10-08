cbb5d8d75a952888e5f6e70a9881d12d6fca8a3f
sujal kumar <sujalkumar0444@gmail.com>
2024-10-09 04:58:22 +0530

    vercel3

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express')
+const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-//dbconnect
-const {databaseconnect}=require("./dbconfig");
-
+// dbconnect
+const { databaseconnect } = require("./dbconfig");
 
 // dbconnection
 databaseconnect();
@@ -24,42 +23,46 @@ const addName = require('./router/user/addName');
 // cors
 app.use(cors({
     origin: '*'
-}))
+}));
 
 app.use(cors({
     origin: 'https://frontend-mauve-tau.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type'],
 }));
+
 // parsers
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true ,limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+
+// Set a timeout for the server (in milliseconds)
+const TIMEOUT = 300000; // 5 minutes (300,000 milliseconds)
+const server = app.listen(process.env.PORT || 8800, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// Increase timeout
+server.setTimeout(TIMEOUT);
 
 // routes
-app.get('/',isAdmin,(req,res)=>{
+app.get('/', isAdmin, (req, res) => {
     res.send("Welcome to the server");
 });
 
-app.use('/register',userRegister);
-app.use('/login',userLogin);
-app.use('/get/quotes',isValidUser,quote);
-app.use('/add/quote',isAdmin,addquote);
-app.use('/get/todaysquote',isValidUser,getTodaysQuote);
-app.use('/edit/todaysquote',isAdmin,editTodaysQuote);
-app.use('/get/name',isValidUser,getName);
-app.use('/add/name',isAdmin,addName);
-
+app.use('/register', userRegister);
+app.use('/login', userLogin);
+app.use('/get/quotes', isValidUser, quote);
+app.use('/add/quote', isAdmin, addquote);
+app.use('/get/todaysquote', isValidUser, getTodaysQuote);
+app.use('/edit/todaysquote', isAdmin, editTodaysQuote);
+app.use('/get/name', isValidUser, getName);
+app.use('/add/name', isAdmin, addName);
 
 app.use((err, req, res, next) => {
-    console.error(err.stack); 
+    console.error(err.stack);
     res.status(err.status || 500).json({
-      error: {
-        message: err.message || 'Internal Server Error'
-      }
+        error: {
+            message: err.message || 'Internal Server Error'
+        }
     });
-  });
-
-const PORT = process.env.PORT || 8800;
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
 });
