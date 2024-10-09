fe03aa2379e1f84f16db118ef50742fdfa9fcedd
sujal kumar <sujalkumar0444@gmail.com>
2024-10-09 11:57:57 +0530

    small changes

--- dbconfig.js
+++ dbconfig.js
@@ -1,9 +1,9 @@
 const mongoose=require("mongoose")
-const {MONGO_URL}=require("./config")
+// const {MONGO_URL}=require("./config")
 async function databaseconnect(){
     try{
         console.log("trying to connect");
-        let data=await mongoose.connect(MONGO_URL);
+        let data=await mongoose.connect(process.env.MONGO_URL);
         console.log("connected to mongo");
     }catch(err){
         console.log(err)
